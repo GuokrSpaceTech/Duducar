cec3e5bca68f14ffd2bdaadce6ee32c1c9951c47
Yang Kai <qch1970@gmail.com>
2015-11-16 17:35:02 +0800

    Added iOS Client initial code, included BaiduMap SDK

---
+++ duducar_passengar_ios/duducar/LoginViewController.h
@@ -0,0 +1,15 @@
+//
+//  LoginViewController.h
+//  duducar
+//
+//  Created by mactop on 11/16/15.
+//  Copyright © 2015 guokrspace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "RCAnimatedImagesView.h"
+#import "RCUnderlineTextField.h"
+
+@interface LoginViewController : UIViewController<RCAnimatedImagesViewDelegate>
+
+@end
