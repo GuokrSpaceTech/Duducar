1483217223da4eca32bc3d15f2ef812dc4b3f3db
Yang Kai <qch1970@gmail.com>
2015-12-11 21:13:47 +0800

    增加估价，支付ViewController

---
+++ duducar_passengar_ios/duducar/PaymentViewController.h
@@ -0,0 +1,13 @@
+//
+//  PaymentViewController.h
+//  duducar
+//
+//  Created by mactop on 12/11/15.
+//  Copyright © 2015 guokrspace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PaymentViewController : UIViewController
+
+@end
