e9ac3040d9bb3bdab14d3530618a81caa2cc9e9b
Yang Kai <qch1970@gmail.com>
2015-12-11 22:58:41 +0800

    增加RatingViewController界面 WIP

---
+++ duducar_passengar_ios/duducar/RatingViewController.h
@@ -0,0 +1,13 @@
+//
+//  RatingViewController.h
+//  duducar
+//
+//  Created by mactop on 12/11/15.
+//  Copyright © 2015 guokrspace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RatingViewController : UIViewController
+
+@end
