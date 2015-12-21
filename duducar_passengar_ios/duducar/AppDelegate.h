541170acfdbe6599c482b7fa8cfefe90f7b1dec1
Yang Kai <yangkai@mac-top.mobi>
2015-12-21 18:11:49 +0800

    修改最低适配版本为7.1

--- duducar_passengar_ios/duducar/AppDelegate.h
+++ duducar_passengar_ios/duducar/AppDelegate.h
@@ -8,7 +8,6 @@
 
 #import <UIKit/UIKit.h>
 #import <BaiduMapAPI_Base/BMKBaseComponent.h>
-@import CocoaAsyncSocket;
 
 @interface AppDelegate : NSObject <UIApplicationDelegate, BMKGeneralDelegate> {
     UIWindow *window;
